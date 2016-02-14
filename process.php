eb112b90a4645d5bbc3b8f0f2d3bcc736f87275b
Keerthi Shekar G <keerushek@gmail.com>
2016-02-15 00:25:15 +0530

    Added new files
    Website Files added

---
+++ process.php
@@ -0,0 +1,63 @@
+<?php
+
+$errors         = array();  	// array to hold validation errors
+$data 			= array(); 		// array to pass back data
+
+// validate the variables ======================================================
+	// if any of these variables don't exist, add an error to our $errors array
+
+
+	if (empty($_POST['name']))
+		$errors['name'] = 'Name is required.';
+
+	if (empty($_POST['email']))
+		$errors['email'] = 'Email is required.';
+
+	if (empty($_POST['mobile']))
+		$errors['mobile'] = 'Mobile is required.';
+	
+	if (empty($_POST['fromDate']))
+		$errors['fromDate'] = 'From date is required.';
+	
+	if (empty($_POST['toDate']))
+		$errors['toDate'] = 'To date is required.';
+	
+	if (empty($_POST['guests']))
+		$errors['guests'] = 'Guests is required.';
+	
+	if (empty($_POST['rooms']))
+		$errors['rooms'] = 'Rooms is required.';
+
+// return a response ===========================================================
+
+	// if there are any errors in our errors array, return a success boolean of false
+	if ( ! empty($errors)) {
+
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+
+		// if there are no errors process our form, then return a message
+
+		// DO ALL YOUR FORM PROCESSING HERE
+		// THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+
+		// show a message of success and provide a true success variable
+		$to = "keerushek@gmail.com";
+		$subject = "This is subject";
+		$txt = "From: " . $_POST['name'] ."\nEmail: ". $_POST['email']."\nMobile: ". $_POST['mobile']."\nFrom Date: ". $_POST['fromDate']."\nTo Date: ". $_POST['toDate']."\nGuests: ". $_POST['guests']."\nRooms: ". $_POST['rooms'];
+		$headers = "From: " . $_POST['name'];
+		$sendMail = mail($to, $subject, $txt, $headers);
+
+		if ($sendMail){
+		  $data['success'] = true;
+		  $data['message'] = 'Success!';
+		} else {
+        	  $data['success'] = true;
+		  $data['message'] = 'Success!';
+                }
+	}
+
+	// return all our data to an AJAX call
+	echo json_encode($data);
---
+++ script/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	 $("#booking-button, #booking-button2").click(function(e){
+        $("#booking-popup").fadeIn(250);
+		 e.stopPropagation();
+    });
+	$(".ar-booking__popup").click(function(e){
+		 e.stopPropagation();
+    });
+    $("body").click(function(){
+        $("#booking-popup").fadeOut(500);
+    });
+    $('form').on('submit', function(e){
+        console.log(e)
+        e.preventDefault()
+    });
+
+    window.submitform = function(){
+        var form_data = $('form').serializeArray();
+
+        console.log("form_data", form_data);
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'process.php', // the url where we want to POST
+            data        : form_data, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+            encode      : true,
+            success:function(data) {
+                $('form')[0].reset();
+                alert("Thank you, you will be contacted by our team very soon")
+                return data; 
+            },
+            error: function(error){
+                console.log(error);
+                alert("Sorry was unable to send you send your request at this time, please try again");
+            }
+        })
+
+    }
+});
