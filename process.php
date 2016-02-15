f65b6fdd2e6d391caa2fa17dbe64c59c1b660f9b
Keerthi Shekar G <keerushek@gmail.com>
2016-02-16 00:11:59 +0530

    reverted PHP back to normal

--- process.php
+++ process.php
@@ -37,42 +37,26 @@ $data 			= array(); 		// array to pass back data
 		$data['success'] = false;
 		$data['errors']  = $errors;
 	} else {
- $smtpoptions = array(
-                        "headers" => GetEmailUserAgent("Thunderbird"),
-                        "textmessage" => $message,
-                        "server" => $smtpserver,
-                        "port" => $smtpport,
-                        "secure" => $reportinfo["smtpsecure"],
-                        "username" => $reportinfo["guna@airportgg.com"],
-                        "password" => $reportinfo["navaneet1947"],
-                        "usedns" => false
-                    );
-...
 
-                        $result = SendEmail("keerushek@gmail.com", "keerthi.shekar@razorthink.com", "Testing", $smtpoptions);
-                        if (!$result["success"])
-                        {
-...I'd make a POP3 connection here and then retry the SMTP stuff - some hosts do POP before SMTP...
-                        }
 		// if there are no errors process our form, then return a message
 
 		// DO ALL YOUR FORM PROCESSING HERE
 		// THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
 
 		// show a message of success and provide a true success variable
-		//$to = "keerthi.shekar@razorthink.com";
-		//$subject = "This is subject";
-		//$txt = "From: " . $_POST['name'] ."\nEmail: ". $_POST['email']."\nMobile: ". $_POST['mobile']."\nFrom Date: ". $_POST['fromDate']."\nTo Date: ". $_POST['toDate']."\nGuests: ". $_POST['guests']."\nRooms: ". $_POST['rooms'];
-		//$headers = "From: " . $_POST['name'];
-		//$sendMail = mail($to, $subject, $txt, $headers);
-//mail($email_to='guna@airportgg.com', $email_subject='test', $email_message='test message');
-		//if ($sendMail){
-		  //$data['success'] = true;
-		  //$data['message'] = 'Success!';
-		//} else {
-        	//  $data['success'] = true;
-		  //$data['message'] = 'Success!';
-            //    }
+		$to = "keerushek@gmail.com";
+		$subject = "This is subject";
+		$txt = "From: " . $_POST['name'] ."\nEmail: ". $_POST['email']."\nMobile: ". $_POST['mobile']."\nFrom Date: ". $_POST['fromDate']."\nTo Date: ". $_POST['toDate']."\nGuests: ". $_POST['guests']."\nRooms: ". $_POST['rooms'];
+		$headers = "From: " . $_POST['name'];
+		$sendMail = mail($to, $subject, $txt, $headers);
+
+		if ($sendMail){
+		  $data['success'] = true;
+		  $data['message'] = 'Success!';
+		} else {
+        	  $data['success'] = true;
+		  $data['message'] = 'Success!';
+                }
 	}
 
 	// return all our data to an AJAX call
